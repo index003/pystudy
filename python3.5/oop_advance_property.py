366a496198aee457ae7fc6d6eaba777f5fc84d20
index003 <index003@sina.cn>
2021-07-08 10:01:40 +0800

    update property

--- python3.5/oop_advance_property.py
+++ python3.5/oop_advance_property.py
@@ -22,6 +22,13 @@ class Student(object):
 '''
 # 上面的调用方法又略显复杂
 # Python内置的@property装饰器就是负责把一个方法变成属性调用的
+'''
+把一个getter方法变成属性，只需要加上@property就可以了，
+此时，@property本身又创建了另一个装饰器@score.setter，
+负责把一个setter方法变成属性赋值，
+于是，我们就拥有一个可控的属性操作
+'''
+
 class Student(object):
 
     @property
