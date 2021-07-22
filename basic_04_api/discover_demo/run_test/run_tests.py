9094351fa3e83de52f623bbb8bf94c073c361201
index003 <index003@sina.cn>
2021-07-22 09:56:12 +0800

    debug

--- basic_04_api/discover_demo/run_test/run_tests.py
+++ basic_04_api/discover_demo/run_test/run_tests.py
@@ -17,19 +17,20 @@ class TestMethod(unittest.TestCase):
     if __name__=='__main__':
 
         sys.path.append('./test_case')    
-        test_dir = './test_case'       #指定当前文件夹下的Interface目录
+        test_dir = './test_case'       
         file = unittest.defaultTestLoader.discover(test_dir, pattern='test*.py')    #匹配开头为test的py文件
         now = time.strftime("%Y-%m-%d %H-%M-%S")
         public_path = os.path.dirname(__file__)  # 当前执行文件的路径 推荐使用这种方式
-        reportpath = '../report' + now + 'report.html'
-        filename = os.path.join(public_path,reportpath)
+        reportpath1 = '../report/'
+        reportpath2 =  now + 'report.html'
+        filename = os.path.join(public_path, reportpath1, reportpath2)
         print(filename)
 
         with open(filename, 'wb') as wf:
             # 定义测试报告
             runner = HTMLTestRunner(
-                stream=wf,
-                title="get post HTMLTestRunner",
-                description="description"
+                stream = wf,
+                title = "get post HTMLTestRunner",
+                description = "description"
             )
             runner.run(file)
